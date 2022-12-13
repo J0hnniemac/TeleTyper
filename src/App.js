1f9f07a7022b93463117764c6afd04ecaf6bfa10
John McManus <J0hnniemac@users.noreply.github.com>
2022-12-13 21:43:12 +0000

    Update App.js
    added clickhandler to app

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import {openingText} from './components/TeleTyperData'
 
 
 function App() {
-  
+  const teleTypeClickHandler = () => {
+    console.log("You clicked the text");
+  }
   return (
     <div>
-      <TeleTyper text={openingText} characterDelay={100}/>
+      <TeleTyper text={openingText} characterDelay={100} teleTypeClickHandler={teleTypeClickHandler} />
     </div>
   );
 }
